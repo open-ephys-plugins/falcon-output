9ba0625794ff9806a06d78396af85d00e48de390
marinechaput <marine.chaput@nerf.be>
2021-09-20 15:42:59 +0200

    Add a C++ client example

---
+++ clients/C++/README.md
@@ -0,0 +1,33 @@
+
+
+# How to run the example
+
+```
+cd clients/build
+mkdir build & cd build
+cmake ..
+make
+./Client
+```
+
+In parallel, open Open-Ephys, add a Stream Plugin node and start the acquisition.
+
+## Expected output 
+
+```
+Received a packet numero 156 (OE packet timestamps = 719665) with 4643 samples from 16 channels.
+Received a packet numero 157 (OE packet timestamps = 724308) with 4643 samples from 16 channels.
+Received a packet numero 158 (OE packet timestamps = 728951) with 4643 samples from 16 channels.
+Received a packet numero 159 (OE packet timestamps = 733594) with 4643 samples from 16 channels.
+Received a packet numero 160 (OE packet timestamps = 738237) with 4643 samples from 16 channels.
+Received a packet numero 161 (OE packet timestamps = 742880) with 4643 samples from 16 channels.
+Received a packet numero 162 (OE packet timestamps = 747523) with 4643 samples from 16 channels.
+Received a packet numero 163 (OE packet timestamps = 752166) with 4643 samples from 16 channels.
+Received a packet numero 164 (OE packet timestamps = 756809) with 4643 samples from 16 channels.
+Received a packet numero 165 (OE packet timestamps = 761452) with 4643 samples from 16 channels.
+Received a packet numero 166 (OE packet timestamps = 766095) with 4643 samples from 16 channels.
+Received a packet numero 167 (OE packet timestamps = 770738) with 4643 samples from 16 channels.
+Received a packet numero 168 (OE packet timestamps = 775381) with 4643 samples from 16 channels.
+Received a packet numero 169 (OE packet timestamps = 780024) with 4643 samples from 16 channels.
+...
+```
---
+++ clients/C++/client.cpp
@@ -0,0 +1,86 @@
+/*
+ ------------------------------------------------------------------
+ StreamPlugin
+ Copyright (C) 2021 - present Neuro-Electronics Research Flanders
+
+ This file is part of the Open Ephys GUI
+ Copyright (C) 2016 Open Ephys
+ ------------------------------------------------------------------
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+ */
+
+#include <zmq.hpp>
+#include <iostream>
+#include <string>
+#include "channel_generated.h"
+#include "flatbuffers/flatbuffers.h"
+
+
+int main(int argc, char **argv) {
+
+    // Parameters
+    std::string address = "127.0.0.1";
+    int port = 3335;
+
+    // Step 1: Create your ZMQ socket
+    zmq::context_t context;
+    auto tcp_address = "tcp://" + address + ":" + std::to_string(port);
+    zmq::socket_t socket = zmq::socket_t(context, ZMQ_SUB);
+    zmq_setsockopt(socket, ZMQ_SUBSCRIBE, nullptr, 0);
+    socket.connect(tcp_address);
+
+    // Step 2 : Loop to receive packets
+
+    while(1){
+
+      zmq_msg_t message;
+      zmq_msg_init (&message);
+      const openephysflatbuffer::ContinuousData* data;
+
+      if (zmq_msg_recv(&message, socket, ZMQ_DONTWAIT) != -1) {   // Non-blocking to wait to receive a message
+
+      // Step 3: Decode the message
+          try {
+              data = openephysflatbuffer::GetContinuousData(zmq_msg_data(&message));
+          } catch (...) {
+              std::cout << "Impossible to parse the packet received - skipping to the next." << std::endl;
+              continue;
+          }
+
+
+          std::cout << "Received a packet numero " << data->message_id()
+                    << " (OE packet timestamps = " << data->timestamp()
+                    << ") with " << data->n_samples()
+                    << " samples from " << data->n_channels() << " channels." << std::endl;
+
+          // Step 4: Process your data: [sample0/chan0, sample1/chan0, ..., sampleN/chan0, sample0/chan1, sample1/chan1...]
+
+          auto data_in_start_iter = data->samples()->begin();
+          auto data_start_channel2 = data->samples()->begin() + 2* data->n_samples();
+          // process
+
+
+
+       }
+
+
+
+    }
+
+
+
+
+}
