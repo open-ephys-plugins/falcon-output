f3ae9959be7c58efec75e6d3f8ae2fa2e4350939
Josh Siegle <joshs@alleninstitute.org>
2025-06-17 13:15:52 -0700

    Bump plugin version to 1.0.1

--- Source/OpenEphysLib.cpp
+++ Source/OpenEphysLib.cpp
@@ -51,7 +51,7 @@ extern "C" EXPORT void getLibInfo (Plugin::LibraryInfo* info)
     info->name = "Falcon I/O";
 
     //Version of the library, used only for information
-    info->libVersion = "1.0.0";
+    info->libVersion = "1.0.1";
     info->numPlugins = NUM_PLUGINS;
 }
 
